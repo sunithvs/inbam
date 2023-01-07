88bcb1412db5776db71bb7f90bf1511d4c373b70
sanumuhammedc <sanumuhammed.c@gmail.com>
2023-01-08 01:24:52 +0530

    static

--- payment/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from rest_framework import routers
-from . import views
-
-urlpatterns = [
-    path("", views.homepage, name="homepage"),
-    path("callback/", views.payment_handler, name="payment_handler"),
-    path("checkout/", views.CheckoutView.as_view(), name="checkout"),
-]
