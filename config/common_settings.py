e963aae6488215a857ed9616fd1f1301cfdc7ada
sunithvs <sunithvs2002@gmail.com>
2023-01-15 11:04:22 +0530

    fixing authentication

--- config/common_settings.py
+++ config/common_settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+
     # django apps
     'django.contrib.admin',
     'django.contrib.auth',
