a1534f2bfc0ff31d41e70de6e928cf7d0e32b89a
sunith vs <sunithvs2002@gmail.com>
2023-02-26 15:01:19 +0530

    working on order checkout

--- home/forms.py
+++ home/forms.py
@@ -1,2 +1,9 @@
 # form of order model
 from django import forms
+from .models import Address
+
+
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = Address
+        fields = ["description", ]
